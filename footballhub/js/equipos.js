a9e13dbd8b4e7529aa834b02ff71b7e632ced80a
joanramz2005 <joanramz2005@gmail.com>
2026-02-11 18:14:57 +0100

    Seccion de filtrar equipos arreglado

--- footballhub/js/equipos.js
+++ footballhub/js/equipos.js
@@ -371,3 +371,33 @@ class TeamsManager {
 document.addEventListener('DOMContentLoaded', () => {
     new TeamsManager();
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    let lastScroll = 0;
+    const header = document.querySelector(".header");
+
+    if (!header) return;
+
+    window.addEventListener("scroll", () => {
+        const currentScroll = window.scrollY;
+
+        // arriba del todo → siempre visible
+        if (currentScroll <= 0) {
+            header.classList.remove("hide");
+            lastScroll = 0;
+            return;
+        }
+
+        // bajando
+        if (currentScroll > lastScroll) {
+            header.classList.add("hide");
+        } 
+        // subiendo
+        else {
+            header.classList.remove("hide");
+        }
+
+        lastScroll = currentScroll;
+    });
+});
